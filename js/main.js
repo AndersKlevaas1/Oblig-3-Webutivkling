b7c9a4209b2bf31dfa64f6c3f3f66624d0739b7f
AndersKlevaas1 <anderkl@hiof.no>
2023-11-09 17:30:41 +0100

    Oblig 4
    Gjort ferdig del 2, klar for å laste opp i filezilla for testing

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let x = document.querySelector(".hamburgernav");
+        let hamburgerButton = document.getElementById("HamburgerMenu");
+
+        hamburgerButton.addEventListener("click", (e) => {
+            if (x.style.display === "block") {
+                x.style.display = "none";
+                hamburgerButton.setAttribute("aria-expanded", "false");
+            } else {
+                x.style.display = "block";
+                hamburgerButton.setAttribute("aria-expanded", "true");
+            }
+        });
